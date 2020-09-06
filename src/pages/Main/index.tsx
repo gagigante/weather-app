956c04b244817ed1cbe364b5f660e4446fabe085
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-09-06 19:52:25 -0300

    Main screen

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { useRoute } from '@react-navigation/native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Alert, ActivityIndicator, View } from 'react-native';
+import { format } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync,
+} from 'expo-location';
 
-import { Container } from './styles';
+import apiClient from '../../services/apiClient';
+
+import convertKelvinToCelsius from '../../utils/convertKelvinToCelsius';
+
+import IApiResponse from './IApiResponse';
+
+import {
+  Container,
+  Header,
+  GoBackButton,
+  RefreshButton,
+  Content,
+  LocationRow,
+  LocationText,
+  DateTimeRow,
+  DateTimeText,
+  WeatherRow,
+  WeatherImage,
+  WeatherText,
+  WeatherDetails,
+  WeatherDescription,
+  WeatherDetailCard,
+  WeatherDetailCardItem,
+  Info,
+  InfoLabel,
+  InfoData,
+  InfoDivider,
+} from './styles';
 
 interface RouteParams {
   latitude: number;
@@ -11,12 +45,174 @@ interface RouteParams {
 
 const Main: React.FC = () => {
   const route = useRoute();
+  const { goBack } = useNavigation();
 
   const routeParams = route.params as RouteParams;
 
+  const [requestDatetime, setRequestDatetime] = useState(new Date());
+  const [apiData, setApiData] = useState<IApiResponse | null>(null);
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [refreshLoading, setRefreshLoading] = useState(false);
+
+  useEffect(() => {
+    apiClient
+      .get('data/2.5/weather', {
+        params: {
+          lat: routeParams.latitude,
+          lon: routeParams.longitude,
+        },
+      })
+      .then(response => {
+        setApiData(response);
+
+        setIsLoading(false);
+      })
+      .catch(err => {
+        setIsLoading(false);
+
+        Alert.alert('Error');
+      });
+  }, [routeParams]);
+
+  const handleGoBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  const handleUpdateData = useCallback(async () => {
+    setRefreshLoading(true);
+
+    const { granted } = await requestPermissionsAsync();
+
+    let latitude;
+    let longitude;
+
+    if (granted) {
+      const { coords } = await getCurrentPositionAsync({
+        enableHighAccuracy: true,
+      });
+
+      latitude = coords.latitude;
+      longitude = coords.longitude;
+    }
+
+    apiClient
+      .get('data/2.5/weather', {
+        params: {
+          lat: latitude,
+          lon: longitude,
+        },
+      })
+      .then(response => {
+        setApiData(response);
+
+        setRequestDatetime(new Date());
+
+        setRefreshLoading(false);
+      })
+      .catch(() => {
+        setRefreshLoading(false);
+
+        Alert.alert('Error', 'something went wrong try again');
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
   return (
     <Container>
-      <Text>{routeParams.latitude}</Text>
+      <Header>
+        <GoBackButton onPress={handleGoBack}>
+          <MaterialIcons name="arrow-back" size={26} color="#FFF" />
+        </GoBackButton>
+
+        <RefreshButton onPress={handleUpdateData}>
+          {refreshLoading ? (
+            <ActivityIndicator color="#000" />
+          ) : (
+            <MaterialIcons name="refresh" size={26} color="#000" />
+          )}
+        </RefreshButton>
+      </Header>
+
+      <Content>
+        <LocationRow>
+          <MaterialIcons name="location-on" size={18} color="#fff" />
+
+          <LocationText>
+            {apiData && `${apiData.data.name} - ${apiData.data.sys.country}`}
+          </LocationText>
+        </LocationRow>
+
+        <DateTimeRow>
+          <DateTimeText>
+            {format(requestDatetime, 'EEE dd MMMM HH:m', {
+              locale: enUS,
+            })}
+          </DateTimeText>
+        </DateTimeRow>
+
+        <WeatherRow>
+          <WeatherImage
+            source={{
+              uri: `http://openweathermap.org/img/wn/${
+                apiData && apiData.data.weather[0].icon
+              }@2x.png`,
+            }}
+          />
+
+          <WeatherText>
+            {apiData && convertKelvinToCelsius(apiData.data.main.temp)}
+          </WeatherText>
+        </WeatherRow>
+
+        <WeatherDetails>
+          {apiData &&
+            `${convertKelvinToCelsius(
+              apiData.data.main.temp_max,
+            )} / ${convertKelvinToCelsius(
+              apiData.data.main.temp_min,
+            )} Feels like ${convertKelvinToCelsius(
+              apiData.data.main.feels_like,
+            )}`}
+        </WeatherDetails>
+
+        <WeatherDescription>
+          {apiData &&
+            apiData.data.weather[0].description.charAt(0).toUpperCase() +
+              apiData.data.weather[0].description.slice(1)}
+        </WeatherDescription>
+
+        <WeatherDetailCard>
+          <WeatherDetailCardItem>
+            <MaterialIcons name="cloud" size={26} color="#000" />
+
+            <Info>
+              <InfoLabel>Humidity</InfoLabel>
+
+              <InfoData>{apiData && `${apiData.data.main.humidity}%`}</InfoData>
+            </Info>
+          </WeatherDetailCardItem>
+
+          <InfoDivider />
+
+          <WeatherDetailCardItem>
+            <MaterialIcons name="menu" size={26} color="#000" />
+
+            <Info>
+              <InfoLabel>Wind speed</InfoLabel>
+
+              <InfoData>{apiData && `${apiData.data.wind.speed} m/s`}</InfoData>
+            </Info>
+          </WeatherDetailCardItem>
+        </WeatherDetailCard>
+      </Content>
     </Container>
   );
 };
--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -1,5 +1,136 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Platform, Dimensions } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View`
+const windowWidth = Dimensions.get('window').width;
+
+export const Container = styled(LinearGradient).attrs({
+  colors: ['#5C56EF', '#8DD1F6'],
+})`
   flex: 1;
+  padding: 48px 0;
+
+  ${Platform.OS === 'ios' &&
+  css`
+    padding-top: ${getStatusBarHeight() + 56}px;
+  `};
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 32px;
+`;
+
+export const GoBackButton = styled.TouchableOpacity``;
+
+export const RefreshButton = styled(RectButton)`
+  width: 56px;
+  height: 56px;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+  border-radius: 4px;
+  opacity: 0.8;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LocationRow = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LocationText = styled.Text`
+  color: #fff;
+  margin-left: 6px;
+  font-family: 'Poppins_400Regular';
+`;
+
+export const DateTimeRow = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const DateTimeText = styled.Text`
+  margin-top: 12px;
+  font-family: 'Poppins_400Regular';
+  color: #e6e6ff;
+`;
+
+export const WeatherRow = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 16px;
+`;
+
+export const WeatherImage = styled.Image`
+  width: 64px;
+  height: 64px;
+`;
+
+export const WeatherText = styled.Text`
+  font-family: 'Poppins_400Regular';
+  color: #fff;
+  font-size: 56px;
+`;
+
+export const WeatherDetails = styled.Text`
+  font-family: 'Poppins_400Regular';
+  text-align: center;
+  color: #fff;
+`;
+
+export const WeatherDescription = styled.Text`
+  font-family: 'Poppins_400Regular';
+  font-size: 18px;
+  color: #fff;
+`;
+
+export const WeatherDetailCard = styled.View`
+  width: ${windowWidth - 48}px;
+  background: #fff;
+  opacity: 0.8;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 16px;
+  padding: 12px;
+  border-radius: 6px;
+`;
+
+export const WeatherDetailCardItem = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Info = styled.View`
+  margin-left: 8px;
+`;
+
+export const InfoLabel = styled.Text`
+  font-family: 'Poppins_400Regular';
+  color: #636363;
+`;
+
+export const InfoData = styled.Text`
+  font-family: 'Poppins_400Regular';
+`;
+
+export const InfoDivider = styled.View`
+  margin: 0 24px;
+  width: 1px;
+  height: 56px;
+  background: #000;
 `;
