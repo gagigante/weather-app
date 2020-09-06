3ba1d03a710571fc1fe4a0bc55de1fc3e6026b54
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-09-06 19:46:05 -0300

    Passing api key inside axios instance

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'api.openweathermap.org/data/2.5/weather',
+  baseURL: 'http://api.openweathermap.org',
 });
+
+apiClient.defaults.params = {};
+apiClient.defaults.params.appid = '439686b83e15b450921ef8541567a577';
+
+export default apiClient;
